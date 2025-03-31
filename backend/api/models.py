92b2c12142602f70050371d27335ea83568f97c2
wilson <mutidawilz@gmail.com>
2025-03-31 21:05:08 +0300

    Corrections on models and serializers

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,99 +1,98 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Custom user manager
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password = None, **extra_fields):
-        "create and return a user"
         if not email:
             raise ValueError("Email is required!")
         
         email = self.normalize_email(email)
-        extra_fields.setdefault("is_active", True)
-        user = self.model(
-            email = email,
-            **extra_fields
-        )
+        extra_fields.setdefault("is_admin", False)
+        user = self.model(email = email, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using = self._db)
         return user
     
     def create_superuser(self, email, password = None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_admin", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff = True')
-        
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser = True')
-        
         return self.create_user(email, password, **extra_fields)
-
-# Role model
+    
+# role model
 class Role(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
-    
-# Custom user model
-class CustomUser(AbstractBaseUser):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100, unique=True)
+
+# Custom user model 
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    role = models.OneToOneField(Role, on_delete=models.CASCADE, related_name='user')
-    date_joined = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
+    role = models.OneToOneField(Role, on_delete=models.CASCADE, related_name='user', default='patient')
+
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
 
     def __str__(self):
-        return f'{self.email} ({self.role.name})'
-    
-# Patient Model
+        return self.email
+
+# Patient model   
 class Patient(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='patient_profile')
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='patient')
+    phone = models.CharField(max_length=10, unique=True)
     date_of_birth = models.DateField()
-    phone_number = models.CharField(max_length=10)
     address = models.TextField()
 
     def __str__(self):
-        return f'Patient: {self.user.first_name} {self.user.last_name}'
-    
+        return f'{self.user.username} ({self.phone})'
+
+# Specialization model   
 class Specialization(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
+# Specialist model   
 class Specialist(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='specialist_profile')
-    specialization = models.OneToOneField(Specialization, on_delete=models.CASCADE, related_name='specialist')
-    phone_number = models.CharField(max_length=100)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='specialist')
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE, related_name='specialist')
+    phone = models.CharField(max_length=10)
+    date_of_birth = models.DateField()
 
     def __str__(self):
-        return f'Specialist: {self.user.username} a {self.specialization.name}'
-    
+        return f'{self.user.username} ({self.phone})'
+ 
+
+# Disorder model  
 class Disorder(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
-    
+
+# Report model 
 class Report(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    specialist = models.ForeignKey(Specialist, on_delete=models.SET_NULL, null=True)
-    disorder = models.ForeignKey(Disorder, on_delete=models.SET_NULL, null=True, blank=True)
-    diagnosis = models.TextField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='reports')
+    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE, related_name='reports')
+    disorder = models.ForeignKey(Disorder, on_delete=models.CASCADE, related_name='reports')
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Report for {self.patient.user.username} on {self.date_created}'
+        return f'Report for {self.patient.user.username} created on {self.date_created}'
+    
+
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,55 +1,207 @@
 from rest_framework import serializers
 from .models import CustomUser, Patient, Specialist, Role, Specialization, Disorder, Report
+import re
+from django.core.validators import RegexValidator
 
 # Role Serializer
 class RoleSerializer(serializers.ModelSerializer):
+    name = serializers.CharField()
     class Meta:
         model = Role
-        fields = '__all__'
+        fields = ['id', 'name']
 
-# Custom User Serializer
-class CustomUserSerializer(serializers.ModelSerializer):
+    def validate_name(self, name):
+        role_name = ['patient', 'specialist']
+
+        if name not in role_name:
+            raise serializers.ValidationError("Invalid role")
+        return name
+
+# Customuser serializer
+class CustomUSerSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField()
     role = RoleSerializer()
+    password = serializers.CharField(write_only = True)
+    confirm_password = serializers.CharField(write_only = True)
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'role', 'date_joined', 'is_active', 'is_staff']
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'role', 'password', 'confirm_password']
 
-# Patient Serializer
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        # Check if passwords match
+        if password and confirm_password and password != confirm_password:
+            raise serializers.ValidationError("Password Mismatch!")
+        
+        # check password length
+        if len(password) < 8:
+            raise serializers.ValidationError("Password should have more than 8 characters")
+        
+        # password to contain at least one digit
+        if not re.search(r'\d', password):
+            raise serializers.ValidationError("Passwords must contain at least one digit")
+        
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+
+        # fetch role data
+        role_data = validated_data.pop('role')
+        role_name = role_data.get('name')
+
+        role, _ = Role.objects.get_or_create(name = role_name)
+
+        # create customuser
+        user = CustomUser.objects.create_user(role = role, **validated_data)
+        return user
+
+# patient serializer
 class PatientSerializer(serializers.ModelSerializer):
-    user = CustomUserSerializer()
 
+    user = CustomUSerSerializer()    
+    phone = serializers.CharField(validators = [
+        RegexValidator(regex=r'^(01|07)\d{8}$', message="Invalid Phone Number!")
+    ])
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = ['id', 'user', 'phone', 'date_of_birth', 'address']
 
-# Specialization Serializer
+    def validate_phone(self, phone):
+        if len(phone) != 10:
+            raise serializers.ValidationError("Invalid Phone Number!")
+        
+        if not (phone.startswith("01") or phone.startswith("07")):
+            raise serializers.ValidationError("Invalid Phone Number")
+        
+        return phone
+    
+    def create(self, validated_data):
+
+        # fetch user from validated data
+        user_data = validated_data.pop('user')
+        role_data = user_data.pop('role')
+        role_name = role_data.get('name')
+
+        if role_name != 'patient':
+            raise serializers.ValidationError("Invalid role.Please use 'patient' instead!")
+        
+        role, _ = Role.objects.get_or_create(name = role_name)
+
+        # create user
+        user = CustomUser.objects.create_user(role=role, **user_data)
+
+        # create patient
+        patient = CustomUser.objects.create(user = user, **validated_data)
+        return patient
+    
+# specialization Serializer   
 class SpecializationSerializer(serializers.ModelSerializer):
+    name = serializers.CharField()
     class Meta:
         model = Specialization
-        fields = '__all__'
+        fields = ['id', 'name']
 
-# Specialist Serializer
+    def validate_name(self, name):
+        specialization_name = ['dentist', 'nurse', 'doctor']
+        if not name in specialization_name:
+            raise serializers.ValidationError("Invalid Specialization Name!")
+        return name
+
+# specialist serializer
 class SpecialistSerializer(serializers.ModelSerializer):
-    user = CustomUserSerializer()
+    user = CustomUSerSerializer()
     specialization = SpecializationSerializer()
+    phone = serializers.CharField(validators = [
+        RegexValidator(regex=r'^(07|01)\d{8}$', message="Invalid Phone Number!")
+    ])
+    date_of_birth = serializers.DateField()
 
     class Meta:
         model = Specialist
-        fields = '__all__'
+        fields = ['id', 'user', 'specialization', 'phone', 'date_of_birth']
 
-# Disorder Serializer
+    def validate_phone(self, phone):
+        if len(phone) != 10:
+            raise serializers.ValidationError("Invali Phone number")
+        
+        if not (phone.startswith("01") or phone.startswith("07")):
+            raise serializers.ValidationError("Invalid Phone Number!")
+        
+        return phone
+    
+    def create(self, validated_data):
+
+        # fetch user data
+        user_data = validated_data.pop('user')
+        specialization_data = validated_data.pop('specialization')
+
+        # validate specialization
+        specialization_name = specialization_data.get('name')
+        specialization_instance, _ = Specialization.objects.get_or_create(name = specialization_name)
+
+        # validate role
+        role_data = user_data.pop('role')
+        role_name = role_data.get('name').lower()
+
+        if role_name != 'specialist':
+            raise serializers.ValidationError("Invalid role.Please use 'specialist' instead!")
+        
+        role_instance, _ = Role.objects.get_or_create(name='specialist')
+
+        # create custom user
+        user = CustomUser.objects.create_user(role = role_instance, **user_data)
+
+        specialist = Specialist.objects.create(user = user, specialization=specialization_instance, **validated_data)
+        return specialist
+
+# disorder serializer
 class DisorderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Disorder
-        fields = '__all__'
+        fields = ['id', 'name', 'description']
 
-# Report Serializer
+# Report serializer
 class ReportSerializer(serializers.ModelSerializer):
     patient = PatientSerializer()
     specialist = SpecialistSerializer()
     disorder = DisorderSerializer()
+    date_created = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Report
-        fields = '__all__'
+        fields = ['id', 'patient', 'specialist', 'disorder', 'date_created']
+
+    def create(self, validated_data):
+
+        # retreive nested objects
+        patient_data = validated_data.pop('patient')
+        specialist_data = validated_data.pop('specialist')
+        disorder_data = validated_data.pop('disorder')
+
+        # create or retreive patient
+        user_data = patient_data.pop('user')
+        role_data = user_data.pop('role')
+        role, _ = Role.objects.get_or_create(name='patient')
+        user = CustomUser.objects.create_user(role=role, **user_data)
+        patient = Patient.objects.create(user=user, **patient_data)
+
+        # create or retreive specialis
+        specialist_user_data = specialist_data.pop('user')
+        specialization_data = specialist_data.pop('specialization')
+        role_specialist, _ = Role.objects.get_or_create(name = 'specialist')
+        specialist_user = CustomUser.objects.create_user(role = role_specialist, **specialist_user_data)
+        specialization, _ = specialization.objects.get_create(name = specialization_data.get('name'))
+        specialist = Specialist.objects.create(user=specialist_user, specialization = specialization, **specialist_data)
+
+        # create or retreive disorder
+        disorder, _ = Disorder.objects.get_or_create(**disorder_data)
+
+        # create report
+        report = Report.objects.create(patient=patient, specialist=specialist, disorder=disorder, **validated_data)
+        return report
+    
